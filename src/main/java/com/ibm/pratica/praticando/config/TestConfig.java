193e8f763276cb70982679a6f7e1dbb3402f8ecb
Marjory <marjory.matos@ibm.com>
2022-08-01 13:43:09 -0300

    products

--- src/main/java/com/ibm/pratica/praticando/config/TestConfig.java
+++ src/main/java/com/ibm/pratica/praticando/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.ibm.pratica.praticando.model.Category;
 import com.ibm.pratica.praticando.model.Pedido;
+import com.ibm.pratica.praticando.model.Product;
 import com.ibm.pratica.praticando.model.User;
 import com.ibm.pratica.praticando.model.enums.OrderStatus;
 import com.ibm.pratica.praticando.repository.CategoryRepository;
 import com.ibm.pratica.praticando.repository.PedidoRepository;
+import com.ibm.pratica.praticando.repository.ProductRespository;
 import com.ibm.pratica.praticando.repository.UserRespository;
 /*
   Classe de configuracao especifica para teste
@@ -35,6 +37,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRespository productRepository;
+	
 	/*
 	 * Instanciando os objetos e salvando no banco de dados
 	 */
@@ -44,7 +49,11 @@ public class TestConfig implements CommandLineRunner{
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		
+		Product p1 = new Product(null, "nome", "description", 123.2, "");
+		Product p2 = new Product(null, "nome", "description", 123.2, "");
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2));
 		
 		User u1 = new User(null, "Maria", "MariaSocial");
 		User u2 = new User(null, "Marcio", "MarcioSocial");
